eaa433891570ee0e4bc673751f05ad91b085e860
narendra <np9634464272@gmail.com>
2023-01-07 13:04:17 +0530

    serverSetUp Snippet

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+/***
+ * use this routes according to your project setup 
+ * 
+ * 
+ * 
+ * const express = require('express');
+const router = express.Router();
+
+const v1ApiRoutes = require('./v1/index');
+
+router.use('/v1', v1ApiRoutes);
+module.exports = router;
+ * 
+ * 
+ * 
+ * 
+ */
